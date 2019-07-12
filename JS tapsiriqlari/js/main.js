5ea8a5fe1dc7b7a869170fc3bd8d7debbae6b9d1
Misir8 <misiraskerov1@gmail.com>
2019-07-13 00:25:08 +0400

    js tsk2

--- JS tapsiriqlari/js/main.js
+++ JS tapsiriqlari/js/main.js
@@ -16,3 +16,21 @@ function foundNum (x, y){
 foundNum(-5, 3);
 foundNum(5, -3);
 foundNum(0.1, 3);
+
+// Ikinci tapsiriq
+function foundResult (num1, num2){
+    num2 > num1;
+    for (let i = num1; i < num2; i++){
+        if(i%2 == 0){
+         console.log(i);
+        }
+    }
+    let result = 0;
+    for (let k = num1; k < num2; k++){
+        result = result + k;
+    }
+    console.log(result);
+}
+// foundResult(5, 15);
+// foundResult(8, 20);
+// foundResult(28, 36);
